5f4550d65e846a7d6289228881ed87f369aa35d7
Damian Montero <photoca@hotmail.com>
2022-08-09 14:32:53 -0400

    onsubmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title,setTitle] = useState("this is the title")
+  const [description,setDescription] = 
+                              useState("your description")
+  const [author,setAuthor] = useState("todd albert")
+
+  console.log(title)
+
+  function formSubmit(e)  {
+    e.preventDefault();
+    console.log("form submitted")
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={formSubmit}>
+            <h1>Comments</h1>
+
+             {/* here goes the title */}
+            <label>Title</label>
+            <input 
+            type="text" 
+            value={title} 
+            onChange={(e)=>{setTitle(e.target.value)}}
+            />
+            <h3>{title}</h3>
+
+            {/* this is the description */}
+            <label>Description</label>
+            <textarea 
+            value={description}
+            onChange={(e)=>{setDescription(e.target.value)}} />
+         
+            <h3>{description}</h3>
+
+            {/* THIS IS THE AUTHOR */}
+            <label>Author</label>
+            <select value={author}
+            onChange={(e)=> {setAuthor(e.target.value)}}
+            >
+              <option value="" selected>Choose One</option>
+              <option value="todd albert">Doctor Todd</option>
+              <option value="ludwigson" >Ludwigson</option>
+              <option value="other">Other</option>
+            </select>
+            <h3>{author}</h3>
+
+            <button>Submit Form</button>
+
+
+        </form>
     </div>
   );
 }
